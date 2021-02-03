d7f4720d3ed6ae6eb9323009e9d7d557a5c6f9cc
Arturo Suárez <masuareb@hotmail.com>
2021-02-03 17:18:50 -0600

    Corrected path of fragment, corrected invalid XML in block declaration of fragments.html template. Added greeting sample to test.

--- src/main/java/dev/masuareb/thymeleafdemo/FragmentsController.java
+++ src/main/java/dev/masuareb/thymeleafdemo/FragmentsController.java
@@ -1,10 +1,18 @@
 package dev.masuareb.thymeleafdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FragmentsController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
     @GetMapping("/fragments")
     public String getHome() {
         return "fragments";
